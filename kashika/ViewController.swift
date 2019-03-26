67a940fcabdf47eb77af81c9f6665e17b74f268c
mtj0928 <mtj0928@users.noreply.github.com>
2019-03-26 10:06:31 +0900

    Initial Commit

---
+++ kashika/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kashika
+//
+//  Created by 松本淳之介 on 2019/03/26.
+//  Copyright © 2019 JunnosukeMatsumoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
