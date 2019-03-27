faea349e93f5b1a066815e0b6ca0436e94fe595d
mtj0928 <mtj0928@users.noreply.github.com>
2019-03-27 20:56:25 +0900

    add AppRouter

---
+++ kashika/AppRouter.swift
@@ -0,0 +1,17 @@
+//
+//  AppRouter.swift
+//  kashika
+//
+//  Created by 松本淳之介 on 2019/03/27.
+//  Copyright © 2019 JunnosukeMatsumoto. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct AppRouter {
+
+    func createRootViewController() -> UIViewController {
+        return ViewController()
+    }
+}
