edc948f75c27c550521927742a3ad06a7a2cca1b
mtj0928 <mtj0928@users.noreply.github.com>
2019-03-28 11:43:20 +0900

    [Pod] add Pring

--- Podfile
+++ Podfile
@@ -1,7 +1,11 @@
 target 'kashika' do
   use_frameworks!
+  
+  inhibit_all_warnings!
 
   pod 'Firebase/Core'
+  pod 'Pring'
+  
 
   target 'kashikaTests' do
     inherit! :search_paths
---
+++ kashika/Entities/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  kashika
+//
+//  Created by 松本淳之介 on 2019/03/28.
+//  Copyright © 2019 JunnosukeMatsumoto. All rights reserved.
+//
+
+import Foundation
+import Pring
+
+class User: Object {
+}
