3f2a1255911d93817e8763ab0f248bea40d6f539
mtj0928 <mtj0928@users.noreply.github.com>
2019-03-28 20:51:44 +0900

    add properties to User

--- kashika/Entities/User.swift
+++ kashika/Entities/User.swift
@@ -6,8 +6,26 @@
 //  Copyright © 2019 JunnosukeMatsumoto. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import Pring
 
-class User: Object {
+@objcMembers
+final class User: Object {
+    dynamic var name: String = ""
+    dynamic var photo: UIImage?
+
+    override func encode(_ key: String, value: Any?) -> Any? {
+        if key == "photo", let data = photo?.pngData() {
+            return File(data: data, mimeType: .png)
+        }
+        return nil
+    }
+
+    override func decode(_ key: String, value: Any?) -> Bool {
+        if key == "photo", let file = value as? File, let data = file.data {
+            photo = UIImage(data: data)
+            return true
+        }
+        return false
+    }
 }
