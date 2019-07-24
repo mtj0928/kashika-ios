7f2a6fb40b9488bf13e31cde558a2214e548e29d
mtj0928 <mtj0928@users.noreply.github.com>
2019-07-24 21:58:13 +0900

    add addUserButton

--- kashika/VIPER/Views/ViewControllers/FriendList/FriendListViewController.swift
+++ kashika/VIPER/Views/ViewControllers/FriendList/FriendListViewController.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
+fileprivate extension UIColor.AppColor {
+    var greenColor: UIColor {
+        return UIColor(hex: "01A564")
+    }
+}
+
 final class FriendListViewController: UIViewController {
 
     @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var addUserButton: EmphasisButton!
+    @IBOutlet private weak var addUseromSNSButton: EmphasisButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupNavigationBar()
+        setupButton()
     }
 }
 
@@ -26,5 +35,15 @@ extension FriendListViewController {
     private func setupNavigationBar() {
         navigationItem.title = "友達一覧"
         self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    private func setupButton() {
+        addUserButton.setTitle("手動で追加", for: .normal)
+        addUserButton.backgroundColor = UIColor.app.greenColor
+        addUserButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18.0)
+
+        addUseromSNSButton.setTitle("SNSから追加", for: .normal)
+        addUseromSNSButton.backgroundColor = UIColor.app.positiveColor
+        addUseromSNSButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18.0)
     }
 }
