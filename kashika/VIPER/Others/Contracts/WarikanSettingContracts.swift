d08602b0137d7435da5a41d17721e44247b81b06
mtj0928 <mtj0928@users.noreply.github.com>
2020-06-05 08:30:36 +0900

    move WarikanUser

--- kashika/VIPER/Others/Contracts/WarikanSettingContracts.swift
+++ kashika/VIPER/Others/Contracts/WarikanSettingContracts.swift
@@ -10,46 +10,6 @@ import FirebaseStorage
 import RxSwift
 import RxCocoa
 
-class WarikanUser {
-    var value: Int
-    let name: String
-    let iconFile: StorageReference?
-
-    let user: User?
-    let friend: Friend?
-
-    var isEdited = false
-    var isSelected = false
-
-    init(money: Int, user: User) {
-        self.value = money
-        self.name = "私"
-        self.iconFile = nil
-        self.user = user
-        self.friend = nil
-    }
-
-    init(money: Int, friend: Friend) {
-        self.value = money
-        self.name = friend.name
-        self.iconFile = friend.iconFile?.storageReference
-        self.user = nil
-        self.friend = friend
-    }
-
-    func reset() {
-        isSelected = false
-        isEdited = false
-        value = 0
-    }
-}
-
-class WarikanUserWhoHasPaid: WarikanUser {
-}
-
-class WarikanUserWhoWillPay: WarikanUser {
-}
-
 protocol WarikanSettingPresenterProtocol {
     var usersWhoHavePaid: BehaviorRelay<[WarikanUserWhoHasPaid]> { get }
     var usersWhoWillPay: BehaviorRelay<[WarikanUserWhoWillPay]> { get }
